45dbe516b5498c9f6cb692494ab8b575f90b4fcf
SojuSojuSoju <branden.wr23@gmail.com>
2024-10-30 11:42:30 +0800

    Added the models and utils foldeer

---
+++ models/Resource.js
@@ -0,0 +1,11 @@
+class Resource {
+    constructor(name, description, owner) {
+    this.name = name;
+    this.description = description;
+    this.owner = owner;
+    const timestamp = new Date().getTime();
+    const random = Math.floor(Math.random() * 1000);
+    this.id = timestamp + "" + random.toString().padStart(3, '0');
+    }
+    }
+    module.exports = { Resource };
