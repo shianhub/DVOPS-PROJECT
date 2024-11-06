8451057e1d47b0b7bf551047efde076836c2e2a9
SojuSojuSoju <branden.wr23@gmail.com>
2024-11-06 16:20:41 +0800

    Update branden.js

--- public/js/branden.js
+++ public/js/branden.js
@@ -16,29 +16,6 @@ function viewBlogs() {
             console.error("Failed to fetch blogs:", request.status, request.statusText);
         }
     };
-        response = JSON.parse(request.responseText);
-
-        var html = '';
-            if (response.length === 0) {
-                html = '<p class = "no-blog"> No blogs found!</p>';
-            } else {
-        for (var i = 0; i < response.length; i++) {
-            html += `
-                <div class="blog-post">
-                    <h3>${response[i].title}</h3>
-                    <p class="blog-content">${response[i].description}</p>
-                    <p><strong>Created by:</strong> ${response[i].author}</p>
-                    <div class="blog-actions">
-                        <button class="blog actions" onclick="editResource(${JSON.stringify(response[i]).replace(/"/g, '&quot;')})">
-                            <i class="fas fa-wrench"></i>
-                        </button>
-                        <button class="blog actions" onclick="deleteResource(${response[i].id})">
-                            <i class="fas fa-trash-alt"></i>
-                        </button>
-                    </div>
-                </div>`;
-        }
-    }   
 
     request.onerror = function () {
         console.error("Request failed.");
